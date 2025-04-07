043adee2044fa2694058f9b44ec4dd3f0e0e0eb1
ManuelSG117 <116238982+ManuelSG117@users.noreply.github.com>
2025-04-07 12:01:06 -0600

    Commit inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'cross-origin'
+    }
+  }
 })
